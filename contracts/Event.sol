15f75a2f2dfdb19ffb22996396dbb922a359b5f6
Victor Ukok <ukokjnr@gmail.com>
2024-09-13 17:31:56 +0100

    first commit

--- contracts/Event.sol
+++ contracts/Event.sol
@@ -6,7 +6,6 @@ import "./ERC271.sol";
 import "hardhat/console.sol";
 
 contract EventContract {
-
     MyToken mtk = new MyToken();
 
     uint256 eventID;
@@ -33,69 +32,83 @@ contract EventContract {
         UserDetails[] eventAtendees;
     }
 
-    mapping (uint => Event) events;
-
+    mapping(uint => Event) events;
 
     event EventCreated(uint eventId, address creator, string eventName);
     event NewAttendee(uint eventId, address attendee);
 
     // user address -> event ID -> nft ID
-    mapping (address => mapping(uint => string)) attendees;
+    mapping(address => mapping(uint => string)) attendees;
 
-    function createEvent(string memory _name, string memory _venue, string memory _dateAndTime, address _nftUrl) external {
+    function createEvent(
+        string memory _name,
+        string memory _venue,
+        string memory _dateAndTime,
+        address _nftUrl
+    ) external {
         eventID += 1;
 
-    Event storage newEvent = events[eventID];
-    
-            newEvent.name = _name;
-            newEvent.venue = _venue;
-            newEvent.dateAndTime = _dateAndTime;
-            newEvent.nftUrl = _nftUrl;
-            newEvent.eventCreator = msg.sender;
-            newEvent.eventStatus = EventStatus.pending;
-            // newEvent.eventAdmins = address memory[];
-            // newEvent.eventAtendees = address memory[];
-        
+        Event storage newEvent = events[eventID];
+
+        newEvent.name = _name;
+        newEvent.venue = _venue;
+        newEvent.dateAndTime = _dateAndTime;
+        newEvent.nftUrl = _nftUrl;
+        newEvent.eventCreator = msg.sender;
+        newEvent.eventStatus = EventStatus.pending;
+        // newEvent.eventAdmins = address memory[];
+        // newEvent.eventAtendees = address memory[];
 
         emit EventCreated(eventID, msg.sender, _name);
     }
 
-    function registerForAnEvent(string memory _name, uint256 _eventID, string memory _nftUrl) external {
-        require(events[eventID].eventStatus == EventStatus.pending || events[eventID].eventStatus == EventStatus.ongoing, "You can't register for this event anymore.");
+    function registerForAnEvent(
+        string memory _name,
+        uint256 _eventID,
+        string memory _nftUrl
+    ) external {
+        require(
+            events[eventID].eventStatus == EventStatus.pending ||
+                events[eventID].eventStatus == EventStatus.ongoing,
+            "You can't register for this event anymore."
+        );
         // require(mtk(events[eventID].nftUrl).balanceOf(msg.sender) > 0, "No nft found");
 
-        events[_eventID].eventAtendees.push(UserDetails({
-            name: _name,
-            urlAddress: msg.sender
-        }));
+        events[_eventID].eventAtendees.push(
+            UserDetails({name: _name, urlAddress: msg.sender})
+        );
 
         attendees[msg.sender][_eventID] = _nftUrl;
 
         emit NewAttendee(_eventID, msg.sender);
-
     }
 
-    function changeEventStatus(uint _eventID, EventStatus _eventStatus) external {
-
-        require(msg.sender == events[_eventID].eventCreator, "Only owner can take this action");
+    function changeEventStatus(
+        uint _eventID,
+        EventStatus _eventStatus
+    ) external {
+        require(
+            msg.sender == events[_eventID].eventCreator,
+            "Only owner can take this action"
+        );
 
         events[_eventID].eventStatus = _eventStatus;
-
     }
 
-    function getEventDetails(uint _eventID) external view returns (Event memory) {
+    function getEventDetails(
+        uint _eventID
+    ) external view returns (Event memory) {
         return events[_eventID];
     }
 
     function myEventTicket(uint256 _eventID) external view returns (bool) {
         Event storage e = events[_eventID];
         bool inEvent;
-        for(uint256 i = 0; i < e.eventAtendees.length; i++) {
-            if(e.eventAtendees[i].urlAddress == msg.sender) {
+        for (uint256 i = 0; i < e.eventAtendees.length; i++) {
+            if (e.eventAtendees[i].urlAddress == msg.sender) {
                 inEvent = true;
             }
         }
         return inEvent;
     }
-    
 }

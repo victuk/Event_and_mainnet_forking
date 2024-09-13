c8d9d02d542d551644f59063058f68e92a8f9833
Victor Ukok <ukokjnr@gmail.com>
2024-09-13 16:57:35 +0100

    first commit

--- test/MainnetForking.js
+++ test/MainnetForking.js
@@ -1,126 +1,53 @@
-const {
-    time,
-    loadFixture,
-  } = require("@nomicfoundation/hardhat-toolbox/network-helpers");
-  const { anyValue } = require("@nomicfoundation/hardhat-chai-matchers/withArgs");
-  const { expect } = require("chai");
-  
-  describe("Lock", function () {
-    // We define a fixture to reuse the same setup in every test.
-    // We use loadFixture to run this setup once, snapshot that state,
-    // and reset Hardhat Network to that snapshot in every test.
-    async function deployOneYearLockFixture() {
-      const ONE_YEAR_IN_SECS = 365 * 24 * 60 * 60;
-      const ONE_GWEI = 1_000_000_000;
-  
-      const lockedAmount = ONE_GWEI;
-      const unlockTime = (await time.latest()) + ONE_YEAR_IN_SECS;
-  
-      // Contracts are deployed using the first signer/account by default
-      const [owner, otherAccount] = await ethers.getSigners();
-  
-      const Lock = await ethers.getContractFactory("Lock");
-      const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
-  
-      return { lock, unlockTime, lockedAmount, owner, otherAccount };
-    }
-  
-    describe("Deployment", function () {
-      it("Should set the right unlockTime", async function () {
-        const { lock, unlockTime } = await loadFixture(deployOneYearLockFixture);
-  
-        expect(await lock.unlockTime()).to.equal(unlockTime);
-      });
-  
-      it("Should set the right owner", async function () {
-        const { lock, owner } = await loadFixture(deployOneYearLockFixture);
-  
-        expect(await lock.owner()).to.equal(owner.address);
-      });
-  
-      it("Should receive and store the funds to lock", async function () {
-        const { lock, lockedAmount } = await loadFixture(
-          deployOneYearLockFixture
-        );
-  
-        expect(await ethers.provider.getBalance(lock.target)).to.equal(
-          lockedAmount
-        );
-      });
-  
-      it("Should fail if the unlockTime is not in the future", async function () {
-        // We don't use the fixture here because we want a different deployment
-        const latestTime = await time.latest();
-        const Lock = await ethers.getContractFactory("Lock");
-        await expect(Lock.deploy(latestTime, { value: 1 })).to.be.revertedWith(
-          "Unlock time should be in the future"
-        );
-      });
-    });
-  
-    describe("Withdrawals", function () {
-      describe("Validations", function () {
-        it("Should revert with the right error if called too soon", async function () {
-          const { lock } = await loadFixture(deployOneYearLockFixture);
-  
-          await expect(lock.withdraw()).to.be.revertedWith(
-            "You can't withdraw yet"
-          );
-        });
-  
-        it("Should revert with the right error if called from another account", async function () {
-          const { lock, unlockTime, otherAccount } = await loadFixture(
-            deployOneYearLockFixture
-          );
-  
-          // We can increase the time in Hardhat Network
-          await time.increaseTo(unlockTime);
-  
-          // We use lock.connect() to send a transaction from another account
-          await expect(lock.connect(otherAccount).withdraw()).to.be.revertedWith(
-            "You aren't the owner"
-          );
-        });
-  
-        it("Shouldn't fail if the unlockTime has arrived and the owner calls it", async function () {
-          const { lock, unlockTime } = await loadFixture(
-            deployOneYearLockFixture
-          );
-  
-          // Transactions are sent using the first signer by default
-          await time.increaseTo(unlockTime);
-  
-          await expect(lock.withdraw()).not.to.be.reverted;
-        });
-      });
-  
-      describe("Events", function () {
-        it("Should emit an event on withdrawals", async function () {
-          const { lock, unlockTime, lockedAmount } = await loadFixture(
-            deployOneYearLockFixture
-          );
-  
-          await time.increaseTo(unlockTime);
-  
-          await expect(lock.withdraw())
-            .to.emit(lock, "Withdrawal")
-            .withArgs(lockedAmount, anyValue); // We accept any value as `when` arg
-        });
-      });
-  
-      describe("Transfers", function () {
-        it("Should transfer the funds to the owner", async function () {
-          const { lock, unlockTime, lockedAmount, owner } = await loadFixture(
-            deployOneYearLockFixture
-          );
-  
-          await time.increaseTo(unlockTime);
-  
-          await expect(lock.withdraw()).to.changeEtherBalances(
-            [owner, lock],
-            [lockedAmount, -lockedAmount]
-          );
-        });
-      });
-    });
-  });
+import { ethers } from "hardhat";
+const helpers = require("@nomicfoundation/hardhat-network-helpers");
+
+async function main() {
+    const ROUTER_ADDRESS = "";
+    const USDC = "";
+    const USDT = "";
+
+    const TOKEN_HOLDER = "";
+
+    await helpers.impersonateAccount(TOKEN_HOLDER);
+    const impersonatedSigner = await ethers.getSigner(TOKEN_HOLDER);
+
+    const amountOut = ethers.parseUnits("20", 18);
+    const amountInMax = ethers.parseUnits("1000", 6);
+
+    const USDC_Contract = await ethers.getContractAt("IERC20", USDC, impersonatedSigner);
+    const DAI_Contract = await ethers.getContractAt("IERC20", USDT);
+    
+    const ROUTER = await ethers.getContractAt("IUniswapV2Router", ROUTER_ADDRESS, impersonatedSigner);
+
+    await USDC_Contract.approve(ROUTER, amountOut);
+
+    const usdcBal = await USDC_Contract.balanceOf(impersonatedSigner.address);
+    const daiBal = await DAI_Contract.balanceOf(impersonatedSigner.address);
+    const deadline = Math.floor(Date.now() / 1000) + (60 * 10);
+
+    console.log("usdc balance before swap", Number(usdcBal));
+    console.log("USDT balance before swap", Number(daiBal));
+
+    await ROUTER.swapTokensForExactTokens(
+        amountOut,
+        amountInMax,
+        [USDC, USDT],
+        impersonatedSigner.address,
+        deadline
+    );
+
+    const usdcBalAfter = await USDC_Contract.balanceOf(impersonatedSigner.address);
+    const daiBalAfter = await DAI_Contract.balanceOf(impersonatedSigner.address);
+
+    console.log("=========================================================");
+
+    console.log("usdc balance after swap", Number(usdcBalAfter));
+    console.log("USDT balance after swap", Number(daiBalAfter));
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+});
